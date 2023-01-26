bb1a3d9b4e9b89b7980d5cbd008627a36f2217bb
Matteo Cadoni <cadonsweb@gmail.com>
2023-01-26 15:42:30 +0100

    basic structure of the application in thymeleaf and boostrap, basic controller to get pages

--- frontend/ChinookFrontend/src/main/java/ch/supsi/chinookfrontend/ChinookFrontendApplication.java
+++ frontend/ChinookFrontend/src/main/java/ch/supsi/chinookfrontend/ChinookFrontendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChinookFrontendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChinookFrontendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookFrontendApplication.class, args);
+    }
 
 }
